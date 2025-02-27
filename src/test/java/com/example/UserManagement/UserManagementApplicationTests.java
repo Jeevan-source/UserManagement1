d248d80c8a0c9bf8be089f7dcd10c091221ca900
Jeevan-source <jeevannithin.l@hinfinity.in>
2025-02-27 08:16:33 +0530

    Initial commit

---
+++ src/test/java/com/example/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test will verify if the application context loads correctly
+    }
+}
